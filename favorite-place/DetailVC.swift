d78f20bf05b64dd13de3e6cfc6a0c53f9ccb0904
TGrooves-208 <tgrooves.208@gmail.com>
2016-02-17 01:00:11 -0700

    fav coffee spot needs size class to fiz portrait layout

--- favorite-place/DetailVC.swift
+++ favorite-place/DetailVC.swift
@@ -1,28 +1,30 @@
 //
-//  ViewController.swift
+//  DetailVC.swift
 //  favorite-place
 //
-//  Created by Gil Aguilar on 2/16/16.
+//  Created by Gil Aguilar on 2/17/16.
 //  Copyright © 2016 redeye-dev. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class DetailVC: UIViewController {
 
-    @IBOutlet weak var moreButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        moreButton.layer.cornerRadius = 2.0
-        
+
+        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
 
-
+    @IBAction func backPressed(sender: AnyObject) {
+        
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
 }
-
