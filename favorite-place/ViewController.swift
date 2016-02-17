dc750db838491370c8a3b068a1eb864210ed0816
TGrooves-208 <tgrooves.208@gmail.com>
2016-02-17 01:05:45 -0700

    Added notes in main view controller comments

--- favorite-place/ViewController.swift
+++ favorite-place/ViewController.swift
@@ -1,7 +1,11 @@
 //
 //  ViewController.swift
-//  favorite-place
-//
+//  favorite-place as in coffee shop
+//  Basic UI Image layout for the main screen
+//  Rounded the corner of the button could have used a seperate class if needed
+//  Stack View Used for the text layout and header.
+//  Need a size class tweak to fix the layout in landscape mode
+//  Added a back button using a seperate Cocoa Touch file respectively
 //  Created by Gil Aguilar on 2/16/16.
 //  Copyright © 2016 redeye-dev. All rights reserved.
 //
